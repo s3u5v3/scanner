a4f0ef151e9787476a1c13a22afdcb765909af1b
sinno666 <666sweetvanilla@gmail.com>
2021-05-05 09:34:29 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Scanner from './Scanner';
+import Result from './Result';
+import { Button, Modal } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      scanCode: '',
+      modal: false,
+      scanSuccess: false,
+    };
+    this._onDetected = this._onDetected.bind(this);
+    this._toggle = this._toggle.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <Button variant="info" block onClick={this._toggle}>
+          Scan Barcode
+        </Button>
+
+        {this.state.scanSuccess ? (
+          <Result key="scanResult" text={this.state.scanCode} />
+        ) : null}
+        <input id="scanner_result" type="text" value={this.state.scanCode} />
+        <input id="scanner_result" type="text" value={this.state.result} />
+        <input id="scanner_result" type="text" value={this.state.scanSuccess} />
+
+        <Modal show={this.state.modal} onHide={this._toggle}>
+          <Modal.Header closeButton="true" />
+          <Modal.Body>
+            <Scanner handleScan={this._onDetected} />
+          </Modal.Body>
+        </Modal>
+      </div>
+    );
+  }
+
+  _toggle() {
+    this.setState((prevState) => ({
+      modal: !prevState.modal,
+      scanSuccess: false,
+    }));
+  }
+
+  _onDetected(result) {
+    this.setState({
+      modal: false,
+      scanCode: result ? result.codeResult.code : '',
+      scanSuccess: result ? true : false,
+      results: result,
+    });
+  }
 }
 
 export default App;
